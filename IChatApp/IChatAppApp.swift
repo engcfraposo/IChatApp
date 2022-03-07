d199e5bc95e416af706f40a4a1f0fd366e17a0f2
Cláudio Rapôso <engcfraposo@gmail.com>
2022-03-07 13:40:51 -0300

    Initial Commit

---
+++ IChatApp/IChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IChatAppApp.swift
+//  IChatApp
+//
+//  Created by claudio.f.raposo on 07/03/22.
+//
+
+import SwiftUI
+
+@main
+struct IChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
